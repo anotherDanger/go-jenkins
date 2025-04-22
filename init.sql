29d0ccb44c600b34c9c53864c0d1537b35a58c24
Andhika Danger <andhikadanger666@gmail.com>
2025-04-22 11:49:10 +0700

    msyql

--- init.sql
+++ init.sql
@@ -1,4 +1,8 @@
-create if not exists table books(
+create database if not exists perpustakaan_new;
+
+use perpustakaan_new;
+
+create table if not exists(
     id int auto_increment primary key ,
     author varchar(255),
     title varchar(255)
